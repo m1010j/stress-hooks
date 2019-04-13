8c39b2886a08eade1849ed07911aa8ec933dc1bb
Matthias Jenny <matthiascjenny@gmail.com>
2019-04-13 00:25:53 -0400

    Updates

---
+++ precache-manifest.80914a03556af5d1fa39cad7f781001e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "955775c631903c13be19",
+    "url": "/stress-hooks/static/js/runtime~main.1fd83dc2.js"
+  },
+  {
+    "revision": "8ff7ccda37b4ce778b9f",
+    "url": "/stress-hooks/static/js/main.7636d6fc.chunk.js"
+  },
+  {
+    "revision": "2ee544cdcd6d05f27024",
+    "url": "/stress-hooks/static/js/2.6be031b1.chunk.js"
+  },
+  {
+    "revision": "8ff7ccda37b4ce778b9f",
+    "url": "/stress-hooks/static/css/main.0d7011fb.chunk.css"
+  },
+  {
+    "revision": "60fd311189069df0d01bcfa8d559e960",
+    "url": "/stress-hooks/index.html"
+  }
+];
