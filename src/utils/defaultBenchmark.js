7ced2c8a627798a9e7accd91e16f1300e298f407
Matthias Jenny <matthiascjenny@gmail.com>
2019-04-13 14:25:43 -0400

    Rename Fibonacci function

--- src/utils/defaultBenchmark.js
+++ src/utils/defaultBenchmark.js
@@ -1,9 +1,9 @@
 const defaultBenchmark = `
-function nthFib(n) {
+function nthFibonacci(n) {
   if (n <= 0) return 0;
   if (n === 1) return 1;
 
-  return nthFib(n - 1) + nthFib(n - 2);
+  return nthFibonacci(n - 1) + nthFibonacci(n - 2);
 }
 `;
 
