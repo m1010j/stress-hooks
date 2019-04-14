c67b1e4ea934c8800b7535adf448b95f552cca16
Matthias Jenny <matthiascjenny@gmail.com>
2019-04-13 21:36:20 -0400

    Updates

--- precache-manifest.7e8f229c9df23d7e2aa1449b402b120a.js
+++ precache-manifest.7e8f229c9df23d7e2aa1449b402b120a.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/stress-hooks/static/js/runtime~main.1fd83dc2.js"
   },
   {
-    "revision": "56b70949f09ef54d614b",
-    "url": "/stress-hooks/static/js/main.bda5a2c9.chunk.js"
+    "revision": "d0fc7a928a318006f9b1",
+    "url": "/stress-hooks/static/js/main.e85b394b.chunk.js"
   },
   {
     "revision": "1af9dcd774d14448c790",
     "url": "/stress-hooks/static/js/2.3bb9e788.chunk.js"
   },
   {
-    "revision": "56b70949f09ef54d614b",
+    "revision": "d0fc7a928a318006f9b1",
     "url": "/stress-hooks/static/css/main.002391af.chunk.css"
   },
   {
-    "revision": "6119ffd216afa7e92e62a5c15995da26",
+    "revision": "533c895a19f4c02759eb07aec8e09a07",
     "url": "/stress-hooks/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/stress-hooks/precache-manifest.4139f5f7ba6f4a81defec60fdfe30737.js"
+  "/stress-hooks/precache-manifest.7e8f229c9df23d7e2aa1449b402b120a.js"
 );
 
 workbox.clientsClaim();
