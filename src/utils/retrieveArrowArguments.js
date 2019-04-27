060d3fb9a1bb79f403c3df664f14015d7e7d4c4e
Matthias Jenny <matthiascjenny@gmail.com>
2019-04-27 15:26:23 -0400

    Fix argument handling for 0-ary arrow functions

--- src/utils/retrieveArrowArguments.js
+++ src/utils/retrieveArrowArguments.js
@@ -1,11 +1,12 @@
 function retrieveArrowArguments(functionStr) {
   const arrowIdx = functionStr.indexOf('=>');
   if (arrowIdx === -1) return [];
-  const strBeforeArrow = functionStr.slice(0, arrowIdx);
+  const strBeforeArrow = functionStr.slice(0, arrowIdx).replace(/\s/g, '');
   const openParenthesisIdx = strBeforeArrow.indexOf('(');
   if (openParenthesisIdx === -1) return [strBeforeArrow];
   const closeParenthesisIdx = strBeforeArrow.indexOf(')');
   if (closeParenthesisIdx === -1) return [];
+  if (openParenthesisIdx + 1 === closeParenthesisIdx) return [];
   return strBeforeArrow
     .slice(openParenthesisIdx + 1, closeParenthesisIdx)
     .split(',');
